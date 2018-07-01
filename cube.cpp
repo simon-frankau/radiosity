039852375aeed8cecd2ad073e015beaed6e56eae
Simon Frankau <sgf@arbitrary.name>
2018-07-01 21:42:00 +0100

    Intermediate state as I switch to on-the-fly normal calculation

--- cube.cpp
+++ cube.cpp
@@ -18,6 +18,9 @@
 #include <GL/glut.h>
 #endif
 
+#include <iostream>
+#include <cmath>
+
 class Vertex
 {
 public:
@@ -29,6 +32,22 @@ public:
         : p {v.x(), v.y(), v.z()}
     {}
 
+    GLfloat len() const
+    {
+        return std::sqrt(x() * x() + y() * y() + z() * z());
+    }
+
+    Vertex norm() const
+    {
+        GLfloat l = len();
+        return Vertex(x() / l, y() / l, z() / l);
+    }
+
+    Vertex operator-(Vertex const &rhs) const
+    {
+        return Vertex(x() - rhs.x(), y() - rhs.y(), z() - rhs.z());
+    }
+
     GLfloat x() const { return p[0]; }
     GLfloat y() const { return p[1]; }
     GLfloat z() const { return p[2]; }
@@ -36,11 +55,28 @@ public:
     GLfloat p[3];
 };
 
+std::ostream &operator<<(std::ostream &os, Vertex const &v)
+{
+    return os << "(" << v.x() << ", " << v.y() << ", " << v.z() << ")";
+}
+
+Vertex cross(Vertex const &v1, Vertex const &v2)
+{
+    return Vertex(v1.y() * v2.z() - v1.z() * v2.y(),
+                  v1.z() * v2.x() - v1.x() * v2.z(),
+                  v1.x() * v2.y() - v1.y() * v2.x());
+}
+
 GLfloat light_diffuse[] = {1.0, 0.0, 0.0, 1.0};  /* Red diffuse light. */
 GLfloat light_position[] = {1.0, 1.0, 1.0, 0.0};  /* Infinite light location. */
-GLfloat n[6][3] = {  /* Normals for the 6 faces of a cube. */
-    {-1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {1.0, 0.0, 0.0},
-    {0.0, -1.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, -1.0} };
+Vertex n[6] = {  /* Normals for the 6 faces of a cube. */
+    Vertex(-1.0, 0.0, 0.0),
+    Vertex(0.0, 1.0, 0.0),
+    Vertex(1.0, 0.0, 0.0),
+    Vertex(0.0, -1.0, 0.0),
+    Vertex(0.0, 0.0, 1.0),
+    Vertex(0.0, 0.0, -1.0)
+};
 GLint faces[6][4] = {  /* Vertex indices for the 6 faces of a cube. */
     {1, 0, 2, 3}, {3, 2, 6, 7}, {7, 6, 4, 5},
     {5, 4, 0, 1}, {4, 6, 2, 0}, {7, 5, 1, 3} };
@@ -61,9 +97,13 @@ drawBox(void)
 {
     int i;
 
+    std::cout << "drawBox" << std::endl;
     for (i = 0; i < 6; i++) {
         glBegin(GL_QUADS);
-        glNormal3fv(&n[i][0]);
+        Vertex norm = cross(v[faces[i][3]] - v[faces[i][0]],
+                            v[faces[i][1]] - v[faces[i][0]]);
+        std::cout << n[i] << " " << norm << std::endl;
+        glNormal3fv(n[i].p);
         glVertex3fv(v[faces[i][0]].p);
         glVertex3fv(v[faces[i][1]].p);
         glVertex3fv(v[faces[i][2]].p);
@@ -76,6 +116,8 @@ void
 display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glRotatef(5, 1.0, 0.0, 0.0);
+    glRotatef(-5, 0.0, 0.0, 1.0);
     drawBox();
     glutSwapBuffers();
 }
