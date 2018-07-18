28660fa6158790a2cc74ed2ed01ec474d96321e7
Simon Frankau <sgf@arbitrary.name>
2018-07-18 22:38:18 +0100

    Start to move towards analytic weighting calculation

--- weighting.cpp
+++ weighting.cpp
@@ -34,16 +34,47 @@ void projWeights(std::vector<GLfloat> &weights)
     }
 }
 
+// Like projWeights, but axis-aligns things first.
+//
+// First step towards doing an analytic version.
+void projWeights2(std::vector<GLfloat> &weights)
+{
+    // From -1 to +1.
+    GLfloat conv = 2.0 / WEIGHT_RESOLUTION;
+    // Make total come to 1.
+    GLfloat weight = 3.0 / (M_PI * 2.0);
+
+    for (int y = 0; y < WEIGHT_RESOLUTION; ++y) {
+        for (int x = 0; x < WEIGHT_RESOLUTION; ++x) {
+            GLfloat px = (x + 0.5) * conv - 1.0;
+            GLfloat py = (y + 0.5) * conv - 1.0;
+            double dist = sqrt(px * px + py * py);
+
+            Vertex v1 = Vertex(dist - conv/2, -0.5 * conv, 1);
+            Vertex v2 = Vertex(dist + conv/2, -0.5 * conv, 1);
+            Vertex v3 = Vertex(dist - conv/2,  0.5 * conv, 1);
+            v1 = v1.norm(); v2 = v2.norm(); v3 = v3.norm();
+            weights.push_back(weight * cross(v3 - v1, v2 - v1).len());
+        }
+    }
+}
+
 // Quick test that the integral comes out right.
 //
 // TODO: I should really get some cpp_unit, shouldn't I?
 int main(void)
 {
-    std::vector<GLfloat> ws;
+    std::vector<GLfloat> ws, ws2;
     projWeights(ws);
-    GLfloat total = 0.0;
+    projWeights2(ws2);
+    GLfloat total = 0.0, total2 = 0.0, maxError = 0.0;
     for (int i = 0, n = ws.size(); i < n; ++i) {
+        // if (i % WEIGHT_RESOLUTION == 0) std::cout << std::endl;
+        // std::cout << ws[i] << " vs " << ws2[i] << "(" << ws[i] / ws2[i] << ")" << std::endl;
         total += ws[i];
+        total2 += ws2[i];
+        maxError = fmax(maxError, fabs(1 - ws[i] / ws2[i]));
     }
-    std::cout << "Should be about 1.0: " << total << std::endl;
+    std::cout << "Should be about 1.0: " << total << ", " << total2 << std::endl;
+    std::cout << "Max error: " << maxError << std::endl;
 }
